7f9ac7c1372a2d7f93378f7afd56987378bfdb85
Nikolay Karetskiy <nkaretskiy@novakidschool.com>
2023-09-28 19:41:57 +0200

    feature(include-blocks): change config for support ejs templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PATH_SRC = path.join(__dirname, './src/pages');
 const PAGES = fs.readdirSync(PATH_SRC).filter((filename) => filename.endsWith('.html'));
-const dataContent = require('./src/data/content.json');
+const templateParameters = require('./src/data/content.json');
 
 module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'main.[contenthash].js',
     publicPath: '',
   },
   mode: 'development',
@@ -48,6 +48,25 @@ module.exports = {
           'postcss-loader',
         ],
       },
+      {
+        test: /\.ejs$/i,
+        loader: 'html-loader',
+        options: {
+          preprocessor: (content, loaderContext) => {
+            let result;
+
+            try {
+              result = _.template(content)(templateParameters);
+            } catch (error) {
+              loaderContext.emitError(error);
+
+              return content;
+            }
+
+            return result;
+          },
+        },
+      },
     ],
   },
   plugins: [
@@ -55,8 +74,8 @@ module.exports = {
       (page) =>
         new HtmlWebpackPlugin({
           template: `${PATH_SRC}/${page}`,
-          filename: `./${page}`,
-          templateParameters: dataContent,
+          filename: page,
+          templateParameters,
         }),
     ),
     new CleanWebpackPlugin(),
